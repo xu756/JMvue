e9fe76a6b46ab3943caec610d35e6f8929c54177
xu756 <756334744@qq.com>
2022-03-06 00:19:55 +0800

    init

--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element'
-Vue.config.productionTip = false
 
+import axios from 'axios'
+// 配置请求的跟路径
+axios.defaults.baseURL = 'api/'
+Vue.prototype.$http = axios
+Vue.config.productionTip = false
 new Vue({
   router,
   render: h => h(App)
