28d7b717de21bb988547225a4fa4348db71e2f35
xu756 <756334744@qq.com>
2022-03-07 17:56:36 +0800

    1

---
+++ src/components/project/myproject.vue
@@ -0,0 +1,61 @@
+<template>
+  <el-table :data="tableData" style="width: 100%">
+    <el-table-column type="index" label="索引"></el-table-column>
+    <el-table-column prop="date" label="日期" width="180"> </el-table-column>
+    <el-table-column prop="name" label="姓名" width="180"> </el-table-column>
+    <el-table-column prop="address" label="地址"> </el-table-column>
+  </el-table>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      tableData: [
+        {
+          date: "2016-05-03",
+          name: "王小虎", 
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1518 弄",
+          zip: 200333,
+          tag: "家",
+        },
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1518 弄",
+          zip: 200333,
+          tag: "公司",
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1518 弄",
+          zip: 200333,
+          tag: "家",
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          province: "上海",
+          city: "普陀区",
+          address: "上海市普陀区金沙江路 1518 弄",
+          zip: 200333,
+          tag: "公司",
+        },
+      ],
+    };
+  },
+  mounted() {
+    var a = JSON.stringify(this.tableData); //对象转json
+    console.log(a);
+    var b = JSON.parse(a); //json转对象
+    console.log(b);
+  },
+  methods: {},
+};
+</script>
